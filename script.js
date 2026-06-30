faeb5546ab4fd37a5ef0d771984a0c94bec982a5
gemasaurav <gema.saurav@gmail.com>
2026-06-30 07:24:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+function convertText() {
+  const input = document.getElementById("inputText").value;
+  let output = "";
+
+  for (let ch of input) {
+    const upper = ch.toUpperCase();
+
+    if (letterMap[upper]) {
+      let converted = letterMap[upper];
+
+      if (ch === ch.toLowerCase()) {
+        converted = converted.toLowerCase();
+      }
+
+      output += converted;
+    }
+    else if (numberMap[ch]) {
+      output += numberMap[ch];
+    }
+    else {
+      output += ch;
+    }
+  }
+
+  document.getElementById("outputText").value = output;
+}
+
+function clearText() {
+  document.getElementById("inputText").value = "";
+  document.getElementById("outputText").value = "";
+}
 
